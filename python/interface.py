9a5fbd6414444741a99e8e55c5a66d87619584ee
thebluebark <114772264+thebluebark@users.noreply.github.com>
2022-11-04 16:35:50 -0400

    Uploading image to server
    Downloaded some packages and allowed images to be uploaded to server side

--- python/interface.py
+++ python/interface.py
@@ -3,11 +3,10 @@ from ela import convert_to_ela_image
 from prediction import predict_result
 from PIL import Image
 
-#Get the file name of the image sent to the interface
+#Get the image sent to the interface
 sentImage = sys.argv[1]
-print(sentImage)
 
-#Get the original image from this file name
+#Get the original image from this
 imageOriginal = Image.open(sentImage).convert("RGB")
 
 #Get the ELA converted image of the sent image
@@ -15,11 +14,9 @@ imageELA = convert_to_ela_image(sentImage, 90)
 
 #Get the results from putting the image through the trained model
 (prediction, confidence) = predict_result(sentImage)
-print(prediction)
-print(confidence)
-#output = prediction + " " + confidence
+output = prediction + " %" + confidence
 
-
+print(output)
 
 # here the python magic takes place
 # ui.py will be deleted in future
--- staticExpress.js
+++ staticExpress.js
@@ -1,4 +1,8 @@
 var express = require('express')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+const {PythonShell} = require('python-shell')
 
 var app = express()
 
@@ -7,6 +11,23 @@ app.use(express.static('public'))
 app.use('/css', express.static(__dirname + '/public/css'))
 app.use('/js', express.static(__dirname + '/public/js'))
 app.use('/images', express.static(__dirname + '/public/images'))
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+
+var pyInterface = '/python/interface.py'
+var pyshell = new PythonShell(pyInterface)
+
+app.post('/upload', upload.single('image'), (req, res) => {
+	res.send(file.path)
+})
 
 var server = app.listen(80, function () {
     var port = server.address().port
